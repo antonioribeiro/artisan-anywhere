9431eae64d4d0663c627104973a9f067d3b907f6
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2013-10-11 18:48:34 -0300

    add artisan anywhere script

---
+++ artisan.sh
@@ -0,0 +1,60 @@
+ARTISAN_APP=artisan
+
+function main() {
+	ourArtisan $@
+}
+
+function ourArtisan()  {
+ 	runLaravelArtisan $@
+}
+
+function runLaravelArtisan() {
+	findLaravelArtisan
+	if [ "$ARTISAN_APP" != "" ] && [ -f $ARTISAN_APP ]; then 
+		php $ARTISAN_APP $@
+		exit 1
+	fi
+}
+
+function runLaravelArtisan() {
+	findLaravelArtisan
+	if [ "$ARTISAN_APP" != "" ] && [ -f $ARTISAN_APP ]; then 
+		php $ARTISAN_APP $@
+		exit 1
+	fi
+}
+
+function findLaravelArtisan() {
+
+	# get the current directory
+	dir=`pwd`
+	# break it to an array
+	dirs=(${dir//\// })
+	# get the number of items
+	I=${#dirs[@]}
+
+	#
+	while [ $I -gt 0 ]; do 
+		dir=
+		x=$I
+		for current in "${dirs[@]}"; do
+		    dir=$dir/$current
+		    ((x--))
+		    if [ $x = 0 ]; then
+		    	break
+		    fi
+		done
+		if [ -f "$dir/$ARTISAN_APP" ]; then
+			app="$dir/$ARTISAN_APP"
+			break
+		fi
+		((I--))
+	done
+
+	if [[ "$app" != "" ]]; then
+		ARTISAN_APP=$app
+	fi
+
+}
+
+main $@
