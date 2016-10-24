48c7d74c5d35a147d9f3cb3277ee6f9673daf0eb
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2016-10-24 12:09:53 -0200

    Change artisan lookup

--- artisan.sh
+++ artisan.sh
@@ -1,62 +1,40 @@
 ARTISAN_APP=./artisan
 
 function main() {
-    runLaravelArtisan $@
+    runLaravelArtisan "$@"
 }
 
 function runLaravelArtisan() {
-
     # is there an artisan in current folder?
-    if [ ! -f $ARTISAN_APP ]; then 
+    if [ ! -f $ARTISAN_APP ]; then
         findLaravelArtisan
     fi
 
     # artisan was found?
-    if [ "$ARTISAN_APP" != "" ] && [ -f $ARTISAN_APP ]; then 
+    if [ "$ARTISAN_APP" != "" ] && [ -f $ARTISAN_APP ]; then
         echo "Artisan found at $ARTISAN_APP"
         php $ARTISAN_APP $@
         exit 1
     else
         echo "sorry, no artisan found"
     fi
-
 }
 
 function findLaravelArtisan() {
+    #find artisan or stop on /
+    dir=..; until [[ -e $dir/artisan || $dir -ef / ]]; do dir+=/..; done
 
-    # get the current directory
-    dir=`pwd`
+    #convert path to absolute path
+    dir=`cd "$dir"; pwd`
 
-    # break it to an array
-    dirs=(${dir//\// })
+    #make artisan file path
+    app=$dir/artisan
 
-    # get the number of items
-    I=${#dirs[@]}
-
-    # search down for artisan
-    ARTISAN_APP=artisan
-    while [ $I -gt 0 ]; do 
-        dir=
-        x=$I
-        for current in "${dirs[@]}"; do
-            dir=$dir/$current
-            ((x--))
-            if [ $x = 0 ]; then
-                break
-            fi
-        done
-        if [ -f "$dir/$ARTISAN_APP" ]; then
-            app="$dir/$ARTISAN_APP"
-            break
-        fi
-        ((I--))
-    done
-
+    echo $app
     # was it found?
     if [[ "$app" != "" ]]; then
         ARTISAN_APP=$app
     fi
-
 }
 
 main "$@"
